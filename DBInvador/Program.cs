40ed56b026a5c9bb5e93c9d4e67317239524a4a7
Durgesh Chaudhary <dcbhai@gmail.com>
2016-04-23 15:18:01 +0530

    Adding program and config xmls

---
+++ DBInvador/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Techphernalia.DBInvader.DBHandler;
+
+namespace Techphernalia.DBInvader
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new DBInvaderForm());
+			//Application.Run(new DBExplorer(srvr));
+		}
+	}
+}
---
+++ DBInvador/TP_CONSTANTS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Techphernalia.DBInvader
+{
+    class TP_CONSTANTS
+    {
+        private TP_CONSTANTS() { }
+        public const string DURGESH_CHAUDHARY = "Durgesh Chaudhary";
+        public const string TECHPHERNALIA = "techPhernalia";
+        public const string TECHPHERNALIA_COM = "techPhernalia.com";
+        public const string DB_Invader = "DB Invader : from " + TECHPHERNALIA_COM + " : by " + DURGESH_CHAUDHARY;
+
+        public static string GetTitle()
+        {
+            return DB_Invader;
+        }
+    }
+}
