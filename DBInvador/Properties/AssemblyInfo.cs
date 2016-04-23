5edb57bc984a5144f0c36086cba625e1be5a9e05
Durgesh Chaudhary <dcbhai@gmail.com>
2016-04-23 15:15:29 +0530

    Adding Project

---
+++ DBInvador/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DB Invader")]
+[assembly: AssemblyDescription("DB Invader allows you to peek into SQL Servers, Databases and Tables with ease.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Concept and Designed by Durgesh Chaudhary")]
+[assembly: AssemblyProduct("DB Invader")]
+[assembly: AssemblyCopyright("Copyright techPhernalia ©  2016")]
+[assembly: AssemblyTrademark("techPhernalia")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a06902c1-64bf-4803-93e7-c74f4ff33db2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
